19506ed22a12248be6a21d3cdadb353c338e1353
github-action-benchmark <github@users.noreply.github.com>
2024-09-30 15:52:15 +0000

    add statement-distribution-regression-bench (customSmallerIsBetter) benchmark result for a8d8596fd2dc36aa7c4e1bb63536c30fef2855ea

--- bench/statement-distribution-regression-bench/data.js
+++ bench/statement-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1727706601840,
+  "lastUpdate": 1727711535170,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "statement-distribution-regression-bench": [
@@ -12711,6 +12711,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "test-environment",
             "value": 0.04580445533399995,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "65984904+programskillforverification@users.noreply.github.com",
+            "name": "Joseph Zhao",
+            "username": "programskillforverification"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a8d8596fd2dc36aa7c4e1bb63536c30fef2855ea",
+          "message": "Replace lazy_static with LazyLock (#5716)\n\n# Description\n\nclose #5641\n\n---------\n\nCo-authored-by: Bastian Köcher <git@kchr.de>",
+          "timestamp": "2024-09-30T14:15:11Z",
+          "tree_id": "55bcfd578e5eabe1acbd69421a2f59c9d7f933d8",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/a8d8596fd2dc36aa7c4e1bb63536c30fef2855ea"
+        },
+        "date": 1727711510292,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Received from peers",
+            "value": 106.39999999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "Sent to peers",
+            "value": 127.94999999999996,
+            "unit": "KiB"
+          },
+          {
+            "name": "statement-distribution",
+            "value": 0.03524988333999999,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.04454270128999994,
             "unit": "seconds"
           }
         ]
